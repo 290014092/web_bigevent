8df5c38ef656e2904bfdcaf867272b82735e72fa
BigWhiteE <290014092@qq.com>
2020-10-09 16:25:32 +0800

    提交index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,6 +1,25 @@
-$.ajaxPrefilter(function(option){
+$.ajaxPrefilter(function (option) {
+    
     // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
     // 会先调用 ajaxPrefilter 这个函数
     // 在这个函数中，可以拿到我们给Ajax提供的配置对象
     option.url = 'http://ajax.frontend.itheima.net' + option.url;
+    // 如果请求中携带my ，设置请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    option.complete = function(res){
+        // console.log('complete');
+        // console.log(res);
+        if(res.responseJSON.status === 1 && res.responseJSON.message ===  '身份认证失败！'){
+            // 请求失败 强制清除口令
+            localStorage.removeItem('token');
+            // 强制跳转到登录页面
+            location.href = '/login.html';
+        } 
+    }
+    
+    
 })
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+/**
+ * @getUserInfo
+ * 获取用户基本信息
+ */
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        // 无论请求成功与否都会调用complate
+        
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败');
+            }
+            
+            // 调用renderAvatar渲染用户头像
+            renderAvatar(res.data);
+        }
+    });
+}
+getUserInfo();
+/**
+ * @renderAvatar
+ * 渲染用户头像
+ */
+function renderAvatar(userMsg) {
+    // 获取用户昵称
+    let name = userMsg.nickname || userMsg.username;
+    // 设置欢迎文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 按需渲染用户的头像
+    if (userMsg.user_pic !== null) {
+        $('#leftHead').attr('src', user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar')
+            .html(first)
+            .show()
+    }
+} 
+$(function(){
+    // 退出
+    $('#Exited').on('click',function(e){
+        // 阻止a连接默认的跳转事件
+        e.preventDefault();
+        // layui的弹出层
+        layer.confirm('确认退出?', {icon: 3, title:'提示'}, function(index){ 
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token');
+            // 重新跳转到登录页面
+            location.href = '/login.html';
+            // 关闭询问框
+            layer.close(index);
+        });
+        
+    })
+})
